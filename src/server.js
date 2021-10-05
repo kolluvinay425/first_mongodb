1d37e8c940bc35fb2877d930b74bc28dccc8074d
kolluvinay425 <kolluvinay425@gmail.com>
2021-10-05 16:59:19 +0200

    connected mongo db

---
+++ src/server.js
@@ -0,0 +1,17 @@
+import express from "express";
+
+server = express();
+
+mongoose.connect(process.env.MONGO_CONNECTION);
+
+mongoose.connection.on("connected", () => {
+  console.log("Successfully connected to Mongo!");
+  server.listen(port, () => {
+    console.table(listEndpoints(server));
+    console.log(`Server running on port ${port}`);
+  });
+});
+
+mongoose.connection.on("error", (err) => {
+  console.log(err);
+});
